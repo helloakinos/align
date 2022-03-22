685271074cb2e9cab421c0eb6fed4194eb8e1953
VinBes <vincentbesuyen@gmail.com>
2022-03-22 10:17:01 +0800

    finderrouter WIP

--- Routers/ViewRouter.js
+++ Routers/ViewRouter.js
@@ -1,38 +1,41 @@
-const express = require("express");
+const { router } = require("../Index.js");
 const isLoggedIn = require("../authFuncs/auth.js").isLoggedIn;
-const isLoggedInAdmin = require("../authFuncs/auth.js").isLoggedInAdmin;
 
-class ViewRouter {
+// ================ Router for  job postings ===================
+
+class FinderRouter {
+  constructor(finderService, express) {
+    this.finderService = finderService;
+    this.express = express;
+  }
+
   router() {
-    let router = express.Router();
+    let router = this.express.Router();
     router.get("/", this.getHome.bind(this));
     router.get("/login", this.getLogin.bind(this));
     router.get("/signup", this.getSignup.bind(this));
-    router.get("/todolist", isLoggedIn, this.getTodoList.bind(this));
-    router.get("/admin", isLoggedInAdmin, this.getAdmin.bind(this));
-    router.get("/error", this.getError.bind(this));
+    router.get(
+      "/myfinderprofile",
+      isLoggedIn,
+      this.getFinderProfile.bind(this)
+    );
+
     return router;
   }
 
   getHome(req, res) {
     res.render("home");
   }
-
   getLogin(req, res) {
     res.render("login");
   }
   getSignup(req, res) {
     res.render("signup");
   }
-  getTodoList(req, res) {
-    res.render("todolist");
-  }
-  getAdmin(req, res) {
-    res.send("ADMIN SECRET DATA");
-  }
-  getError(req, res) {
-    res.render("error");
+
+  getFinderProfile(req, res) {
+    res.render("finderProfile");
   }
 }
 
-module.exports = ViewRouter;
+module.exports = FinderRouter;
