d1905aec7dd949c532bce43d13e1515f8fe248db
VinBes <vincentbesuyen@gmail.com>
2022-03-25 16:21:58 +0800

    Additional routings and functionalities for finder profiles

--- Routers/FProfileRouter.js
+++ Routers/FProfileRouter.js
@@ -10,14 +10,14 @@ class FProfileRouter {
   router() {
     let router = this.express.Router();
     router.get("/finderprofile", this.getFinderProfile.bind(this));
-    router.post("/finderprofile", this.postCustomField.bind(this));
-    router.put("/finderprofile/:id", this.put.bind(this));
+    router.post("/finderprofile", this.postCustomField.bind(this)); // needs authentication to even access the edit form
+    router.put("/finderprofile", this.putProfileInfo.bind(this)); // needs authentication to even access the edit form
+    router.put("/finderprofile/:id", this.putCustomField.bind(this));
     router.delete("/finderprofile/:id", this.delete.bind(this));
     return router;
   }
 
   getFinderProfile(req, res) {
-    console.log(req);
     let finderId = req.rawHeaders[1];
     console.log(`Current Finder ID: ${finderId}`);
     return this.finderProfileService
@@ -52,13 +52,34 @@ class FProfileRouter {
       });
   }
 
-  put(req, res) {
+  putProfileInfo(req, res) {
     let info = req.body.info;
-    let finderId = req.auth.finder.finderId;
+    let finderId = req.rawHeaders[1];
     console.log("FProfile Router: PUT Method");
     console.log(`Current Finder ID: ${finderId} and info: ${info}`);
     return this.finderProfileService
       .updateProfile(info, finderId)
+      .then(() => {
+        return this.finderProfileService.listprofile(finderId);
+      })
+      .then((profile) => {
+        res.json(profile);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
+  }
+
+  putCustomField(req, res) {
+    let customfield_Id = req.params.id;
+    let customfieldInfo = req.body.customfieldInfo;
+    let finderId = req.rawHeaders[1];
+    console.log("FProfile Router: PUT Method");
+    console.log(
+      `Current Finder ID: ${finderId} and customfieldInfo: ${customfieldInfo}`
+    );
+    return this.finderProfileService
+      .updateCustom(customfield_Id, customfieldInfo, finderId)
       .then(() => {
         return this.finderProfileService.listprofile(finderId);
       })
