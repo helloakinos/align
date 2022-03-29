daa24cec9c41e43e13e4e431562aaf3440fc911f
Akin Smith <akin.smyth@gmail.com>
2022-03-29 17:09:04 +0800

    seeding in progress

--- migrations/20220319142223_job_form_create.js
+++ migrations/20220319142223_job_form_create.js
@@ -7,7 +7,7 @@ exports.up = function(knex) {
         table.integer("seeker_id");
         table.foreign("seeker_id").references("seeker.seeker_id");
         table.integer("job_id").notNullable().unique().unsigned();;
-        // table.foreign("job_id").references("job.job_id");
+        table.foreign("job_id").references("job.job_id");
         table.primary(["seeker_id", "job_id"]);
         table.string("question1");
         table.string("question2");
